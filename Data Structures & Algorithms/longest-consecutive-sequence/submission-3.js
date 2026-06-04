05b19394fa145604422a75a3c58bc70502018a69
Maruf Ahmed <ahmedmaruf474@gmail.com>
2026-06-04 18:38:00 +0600

    Add: longest-consecutive-sequence - submission-3

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-3.js
@@ -0,0 +1,40 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     * We can use a hashmap to store the previous seen items with
+     * index.
+     * for each item we look for +/- 1 item. when found we can store the
+     * last seen item
+     * 
+     * we have to store both index and the count including the num 
+     * itself.
+     * start 0 - 2
+     * longest 2
+     * 
+     * i start  len longest
+     * 0    0   1   -inf
+     * 1    0   2   -inf
+     * 2    2   1   i - start = 2
+     * 3    2   2   2
+     * 4    4
+     * 5
+     * 6
+     * 7    2   5
+     * 0 1 1 2 3 4 5 6 7
+     */
+    longestConsecutive(nums) {
+        let res = 0
+        let numSet = new Set(nums)
+        for (let num of nums){
+            let currStreak = 1
+            
+            while (numSet.has(++num)){
+                currStreak++
+            }
+            res = Math.max(res, currStreak)
+        }
+        return res
+    }
+    
+}
