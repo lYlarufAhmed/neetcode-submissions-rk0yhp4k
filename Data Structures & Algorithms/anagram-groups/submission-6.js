60368a9026293522e0588d85cc52fe25c4db9dcc
Maruf Ahmed <ahmedmaruf474@gmail.com>
2026-08-19 08:24:24 +0600

    Add: anagram-groups - submission-6

---
+++ Data Structures & Algorithms/anagram-groups/submission-6.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        let m = new Map();
+        strs.map((str, i) => [str.split("").sort().join(""), str])
+            .sort(([s, i], [s1, i1]) => {
+                if (s < s1) {
+                    return -1;
+                }
+                if (s > s1) {
+                    return 1;
+                }
+
+                // names must be equal
+                return 0;
+            })
+            .forEach(([key, val]) => {
+                let curr = m.get(key);
+                if (curr) {
+                    curr.push(val);
+                } else {
+                    curr = [val];
+                }
+                m.set(key, curr);
+            });
+        return Array.from(m.values());
+    }
+}
