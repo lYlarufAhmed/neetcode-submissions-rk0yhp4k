127db0a45a77221fd178d7c64069a81736a14637
Maruf Ahmed <ahmedmaruf474@gmail.com>
2026-08-26 06:39:55 +0600

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.ts
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s: string): boolean {
+        let chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+        let s_l = s.toLowerCase();
+        s.charCodeAt(0);
+        const isAlpha = (code: number) =>
+            (code >= 97 && code <= 122) || (code >= 65 && code <= 90) || (code >= 48 && code <= 57);
+
+        let l = 0;
+        let r = s.length - 1;
+
+        while (l < r) {
+            if (isAlpha(s.charCodeAt(l)) && isAlpha(s.charCodeAt(r))) {
+                if (s_l[l] == s_l[r]) {
+                    l++;
+                    r--;
+                    continue;
+                } else {
+                    return false;
+                }
+            } else if (!isAlpha(s.charCodeAt(l))) l++;
+            else if (!isAlpha(s.charCodeAt(r))) r--;
+        }
+        return true;
+    }
+}
